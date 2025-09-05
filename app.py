f177e7f929f85e24bb2eb4e049642eab0c0915a2
ShivamNishad0 <2k23.mca23136722@gmail.com>
2025-09-06 00:44:46 +0530

    app.py for production

--- app.py
+++ app.py
@@ -153,4 +153,6 @@ def recommend_book():
     return render_template('recommend.html',data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
